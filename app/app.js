c8541a5e6dc1ea9699a3958b4b036a94d4198d9c
m-gibula <gibula27135@student.pwsztar.edu.pl>
2017-05-10 08:47:27 +0200

    findMin implementation

--- app/app.js
+++ app/app.js
@@ -1,6 +1,13 @@
 function findMin(array)
 {
+    var i =0;
+    for(i=0;i<array.length;i++){
+        if(isNaN(array[i])){
+            return false;
+        }
 
+    }
+    return Math.min(array);
 }
 module.exports = {
     findMin: findMin
