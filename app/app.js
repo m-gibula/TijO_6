8fdb7118bf795f26b6a251d9de55361b0eedd894
Kover24 <klich27150@student.pwsztar.edu.pl>
2017-05-10 08:47:23 +0200

    FindMaxFunction

--- app/app.js
+++ app/app.js
@@ -1,6 +1,13 @@
 function findMax(array)
 {
+    var i =0;
+    for(i=0;i<array.length;i++){
+        if(isNaN(array[i])){
+            return false;
+        }
 
+    }
+    return Math.max(array);
 }
 module.exports = {
     findMax: findMax
